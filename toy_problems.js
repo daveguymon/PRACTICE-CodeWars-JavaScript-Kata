999d2aedb336bae3c5bb953704f6891bf1845f9f
Dave Guymon <davidwguymon@gmail.com>
2017-09-08 16:17:02 -0600

    Completed kata to sum (n) fractions.

--- toy_problems.js
+++ toy_problems.js
@@ -1,3 +1,43 @@
+// Task:
+//
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+//
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+//
+// You need to round the answer to 2 decimal places and return it as String.
+// If the given value is 0 then it should return 0.00
+// You will only be given Natural Numbers as arguments.
+// Examples:
+//
+// SeriesSum(1) => 1 = "1.00"
+// SeriesSum(2) => 1 + 1/4 = "1.25"
+// SeriesSum(5) => 1 + 1/4 + 1/7 + 1/10 + 1/13 = "1.57"
+
+function SeriesSum(n){
+  let fractionsArr = [];
+  let instances = 1;
+  let denom = 1;
+
+  while(instances <= n){
+    fractionsArr.push(denom);
+    denom+=3;
+    instances++;
+  }
+
+  let decimals = fractionsArr.map(value => {
+    return 1/value
+  })
+  
+  let sum = decimals.reduce(function(sum, value) {
+    return sum + value;
+  }, 0)
+
+  return sum.toFixed(2);
+}
+
+
+
 // In this kata, you must create a digital root function.
 //
 // A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has two digits, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
