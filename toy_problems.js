4c029c7af87b1a951a1c91e733ea9828558f240d
Dave Guymon <davidwguymon@gmail.com>
2017-09-09 13:58:09 -0600

    Multiples of 3 and 5 kyu 6

--- toy_problems.js
+++ toy_problems.js
@@ -1,3 +1,29 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+//
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number){
+  var numsArr = [];
+
+  for(var i = 3; i < number; i++){
+    numsArr.push(i)
+  }
+
+  var threesFives = numsArr.filter(val => {
+    return val % 3 === 0 || val % 5 === 0;
+  })
+
+  var solution = threesFives.reduce((curr, next) => {
+    return curr += next
+  }, 0)
+
+  return solution;
+}
+
+solution(10) //23
+
 // This time no story, no theory. The examples below show you how to write function accum:
 //
 // Examples:
