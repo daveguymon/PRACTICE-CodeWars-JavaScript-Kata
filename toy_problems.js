8933294838b6d4893380a65e780f40da8544760b
Dave Guymon <davidwguymon@gmail.com>
2017-09-14 16:52:23 -0600

    Finding BMI kyu 8

--- toy_problems.js
+++ toy_problems.js
@@ -1,3 +1,30 @@
+// Write function bmi that calculates body mass index (bmi = weight / height ^ 2).
+//
+// if bmi <= 18.5 return "Underweight"
+//
+// if bmi <= 25.0 return "Normal"
+//
+// if bmi <= 30.0 return "Overweight"
+//
+// if bmi > 30 return "Obese"
+
+function bmi(weight, height) {
+  var mass = weight / (height * height);
+
+  if(mass <= 18.5){
+    return "Underweight";
+  }
+  else if(mass <= 25.0){
+    return "Normal";
+  }
+  else if(mass <= 30.0){
+    return "Overweight";
+  }
+  else {
+    return "Obese";
+  }
+}
+
 // Longest Palindrome
 //
 // Find the length of the longest substring in the given string s that is the same in reverse.
