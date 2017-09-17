1ad887674a7235e9827466818608d652ac12e345
Dave Guymon <davidwguymon@gmail.com>
2017-09-16 22:48:02 -0600

    Mutate my strings kyu-7

--- codewars/kyu-7.js
+++ codewars/kyu-7.js
@@ -1,3 +1,41 @@
+// Mutate My Strings - https://www.codewars.com/kata/mutate-my-strings/javascript
+
+// I will give you two strings. I want you to transform stringOne into stringTwo one letter at a time.
+//
+// Example:
+//
+// stringOne = 'bubble gum';
+// stringTwo = 'turtle ham';
+//
+// Result:
+// bubble gum
+// tubble gum
+// turble gum
+// turtle gum
+// turtle hum
+// turtle ham
+
+function mutateMyStrings(stringOne, stringTwo){
+  var firstString = stringOne.split('');
+  var secondString = stringTwo.split('');
+  var stringsArr = [stringOne];
+
+  for(var i = 0; i <= firstString.length; i++) {
+    if(firstString[i] !== secondString[i]){
+      firstString[i] = secondString[i];
+    }
+    if(stringsArr.indexOf(firstString.join('')) === -1){
+      stringsArr.push(firstString.join(''));
+    }
+  }
+  var solution = stringsArr.map(string => {
+    return string+ "\n";
+  });
+  return String(solution).split(',').join('');
+}
+
+mutateMyStrings('bubble gum', 'turtle ham')
+
 // Get the Middle Character - https://www.codewars.com/kata/get-the-middle-character/train/javascript
 
 // You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
