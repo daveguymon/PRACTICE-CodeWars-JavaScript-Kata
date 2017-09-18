fe0c077b29c2c31ac8c65628cf3dd88493512438
Dave Guymon <davidwguymon@gmail.com>
2017-09-17 22:04:14 -0600

    Stop gninnipS My sdroW

--- kyu-6.js
+++ kyu-6.js
@@ -1,4 +1,23 @@
-//Array.diff -
+// Stop gninnipS My sdroW! - https://www.codewars.com/kata/stop-gninnips-my-sdrow/javascript
+
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+//
+//
+// Examples:
+//
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+// spinWords( "This is a test") => returns "This is a test"
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+function spinWords(str){
+  var solution = str.split(' ').map(word => {
+    return word.length >= 5 ? word.split('').reverse().join('') : word;
+  }).join(' ');
+
+  return solution;
+}
+
+//Array.diff - https://www.codewars.com/kata/523f5d21c841566fde000009
 
 // Your goal in this kata is to implement an difference function, which subtracts one list from another.
 //
