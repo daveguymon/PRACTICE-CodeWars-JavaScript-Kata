b00f74c7de79c42847cc4e2662a8ba884b8791a5
Dave Guymon <davidwguymon@gmail.com>
2017-09-17 22:44:23 -0600

    Is A Number Prime

--- kyu-6.js
+++ kyu-6.js
@@ -1,3 +1,31 @@
+//Is A Number Prime? - https://www.codewars.com/kata/5262119038c0985a5b00029f/solutions/javascript
+
+// Define a function isPrime/is_prime() that takes one integer argument and returns true/True or false/False depending on if the integer is a prime.
+//
+// Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+//
+// Example
+//
+// isPrime(5)
+// => true
+// Assumptions
+//
+// You can assume you will be given an integer input.
+// You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+
+function isPrime(num) {
+  if(num < 2) {
+    return false;
+  } else {
+    for(var i = 2; i < num; i++){
+      if(num % i === 0){
+        return false;
+      }
+    }
+      return true;
+  }
+}
+
 // Stop gninnipS My sdroW! - https://www.codewars.com/kata/stop-gninnips-my-sdrow/javascript
 
 // Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
