9713672756fb33019f8fbf2d97bbd992bd8161ee
Dave Guymon <davidwguymon@gmail.com>
2017-09-19 21:17:12 -0600

    Largest 5-digit Number In A Series kyu-5

--- kyu-5.js
+++ kyu-5.js
@@ -1,3 +1,20 @@
+// Largest 5 digit number in a series - https://www.codewars.com/kata/51675d17e0c1bed195000001/solutions/javascript
+
+// Complete the solution so that it returns the largest five digit number found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
+
+function solution(digits){
+  var numsArr = digits.split('');
+  var largest = 0;
+
+  for(var i = 0; i < numsArr.length-1; i++){
+    if(parseInt(numsArr.slice(i, i+5).join(''), 0) > largest) {
+      largest = parseInt(numsArr.slice(i, i+5).join(''),0);
+    }
+  }
+
+  return largest;
+}
+
 //Simple Pig Latin - https://www.codewars.com/kata/simple-pig-latin/javascript
 
 // Move the first letter of each word to the end of it, then add 'ay' to the end of the word.
