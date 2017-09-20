f3855d1e341f902e1ac158ac2ec36acda7f75465
Dave Guymon <davidwguymon@gmail.com>
2017-09-19 22:08:15 -0600

    Moving Zeros To The End

--- kyu-5.js
+++ kyu-5.js
@@ -1,3 +1,16 @@
+// Moving Zeros To The End - https://www.codewars.com/kata/moving-zeros-to-the-end/javascript
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+//
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+var moveZeros = function (arr) {
+  var nums = arr.filter(num => num !== 0);
+  var zeros = arr.filter(num => num === 0);
+  var solution = nums.concat(zeros);
+  return solution;
+}
+
 // Largest 5 digit number in a series - https://www.codewars.com/kata/51675d17e0c1bed195000001/solutions/javascript
 
 // Complete the solution so that it returns the largest five digit number found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
