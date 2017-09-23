129b759d3288439eaf0c6c304146f058b842b959
Dave Guymon <davidwguymon@gmail.com>
2017-09-22 23:13:44 -0600

    Disemvowell Trolls kyu7

--- kyu-7.js
+++ kyu-7.js
@@ -1,3 +1,17 @@
+//Disemvowell Trolls - https://www.codewars.com/kata/disemvowel-trolls/javascript
+
+// Trolls are attacking your comment section!
+//
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+//
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+//
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '')
+}
+
 //Remove the Minimun - https://www.codewars.com/kata/remove-the-minimum/javascript
 
 // Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
