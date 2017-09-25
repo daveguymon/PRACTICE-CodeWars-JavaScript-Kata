062a6cf101456756ac8d1c19a0222c9986bb38fb
Dave Guymon <davidwguymon@gmail.com>
2017-09-24 22:28:59 -0600

    Find the Odd Int kyu6

--- kyu-6.js
+++ kyu-6.js
@@ -1,3 +1,24 @@
+// Find The Odd Int - http://www.codewars.com/kata/find-the-odd-int/javascript
+
+// Given an array, find the int that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+
+function findOdd(A) {
+  var solution = null;
+
+  var count = A.reduce((tally, num) => {
+    tally[num] = (tally[num] || 0) + 1;
+    return tally;
+  }, {});
+
+  for(var number in count){
+    if(count[number] % 2 !== 0) {
+      solution = number;
+    }
+  }
+  return Number(solution);
+}
+
 // Take A Ten Minute Walk - http://www.codewars.com/kata/take-a-ten-minute-walk/javascript
 
 // You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
