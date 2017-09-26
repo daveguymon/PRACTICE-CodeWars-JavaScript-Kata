cde42c344c99411775ac0d69191b516e3babf6e7
Dave Guymon <davidwguymon@gmail.com>
2017-09-26 13:11:54 -0600

    Odd-heavy Array kyu6

--- kyu-6.js
+++ kyu-6.js
@@ -1,3 +1,32 @@
+// Odd-heavy Array - https://www.codewars.com/kata/odd-heavy-array/javascript
+
+// An array is defined to be odd-heavy if it contains at least one odd element and every element whose value is odd is greater than every even-valued element.
+//
+// eg.
+//
+// Array [11,4,9,2,8] is odd-heavy
+// because:- its odd elements [11,9] are greater than all the even elements [4,2,8]
+//
+// Array [11,4,9,2,3,10] is not odd-heavy
+// because:- one of it's even element 10 from [4,2,10] is greater than two of its odd elements [9,3] from [ 11,9,3]
+// write a function called isOddHeavy that accepts an integer array and returns true if the array is odd-heavy else return false.
+
+function isOddHeavy(n){
+  n = n.sort((a,b) => a-b);
+  var evenIndices = [];
+  var oddIndices = [];
+
+  for(var i = 0; i < n.length; i++){
+   if(n[i] % 2 === 0) {
+     evenIndices.push(i);
+   } else {
+     oddIndices.push(i);
+   }
+  }
+
+  return oddIndices.length && oddIndices[0] > evenIndices[evenIndices.length-1] || n.length === 1 && n[0] % 2 !== 0 || oddIndices.length && !evenIndices.length? true : false;
+}
+
 // Format Words Into a Sentence - https://www.codewars.com/kata/format-words-into-a-sentence/javascript
 
 // Complete the method so that it formats the words into a single comma separated value. The last word should be separated by the word 'and' instead of a comma. The method takes in an array of strings and returns a single formatted string. Empty string values should be ignored. Empty arrays or null/nil values being passed into the method should result in an empty string being returned.
