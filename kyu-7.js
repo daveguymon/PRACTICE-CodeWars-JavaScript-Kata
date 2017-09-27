ddefe1da5ba24dda862af57eaac2b750002a3c74
Dave Guymon <davidwguymon@gmail.com>
2017-09-26 23:17:21 -0600

    Is This A Triangle kyu7

--- kyu-7.js
+++ kyu-7.js
@@ -1,3 +1,13 @@
+// Is This A Triangle? - https://www.codewars.com/kata/is-this-a-triangle/javascript
+
+// Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+//
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+function isTriangle(a,b,c){
+   return a + b > c && b + c > a && a + c > b ? true : false;
+}
+
 // You're A Square - https://www.codewars.com/kata/youre-a-square/javascript
 
 // Given an integral number, determine if it's a square number.
