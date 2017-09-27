2fb5c44fbb79980b387e5051ede539b37e98b9be
Dave Guymon <davidwguymon@gmail.com>
2017-09-27 12:49:51 -0600

    Jaden Casing Strings kyu7

--- kyu-7.js
+++ kyu-7.js
@@ -1,3 +1,18 @@
+// Jaden-casing Strings - http://www.codewars.com/kata/jaden-casing-strings/javascript
+
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
+//
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+//
+// Example:
+//
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+String.prototype.toJadenCase = function () {
+  return this.split(' ').map(word => word.substr(0,1).toUpperCase()+word.substr(1).toLowerCase()).join(' ')
+};
+
 // Complementary DNA - https://www.codewars.com/kata/complementary-dna/javascript
 
 // Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
