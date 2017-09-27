43ad769a3710e8a2fef02ab87e858c7d8ef0fd10
Dave Guymon <davidwguymon@gmail.com>
2017-09-27 12:39:04 -0600

    Complementary DNA kyu7

--- kyu-7.js
+++ kyu-7.js
@@ -1,3 +1,37 @@
+// Complementary DNA - https://www.codewars.com/kata/complementary-dna/javascript
+
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+//
+// If you want to know more http://en.wikipedia.org/wiki/DNA
+//
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+//
+// DNAStrand ("ATTGC") # return "TAACG"
+//
+// DNAStrand ("GTAT") # return "CATA"
+
+function DNAStrand(dna){
+  var solution = [];
+
+  var mapped = dna.split('').map(symbol => {
+    switch(symbol){
+      case 'A':
+        solution.push('T');
+        break;
+      case 'T':
+        solution.push('A');
+        break;
+      case 'C':
+        solution.push('G');
+        break;
+      case 'G':
+        solution.push('C');
+        break;
+    }
+  })
+  return solution.join('');
+}
+
 // Is This A Triangle? - https://www.codewars.com/kata/is-this-a-triangle/javascript
 
 // Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
