1caf84437659f58bb771452fcd318e8472729a34
Dave Guymon <davidwguymon@gmail.com>
2017-09-30 10:59:30 -0600

    List Filtering

--- kyu-7.js
+++ kyu-7.js
@@ -1,3 +1,17 @@
+//List Filtering - https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/solutions/javascript
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+//
+// Example
+//
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+  return l.filter(element => typeof(element) === 'number');
+}
+
 // Reverse Factorials - https://www.codewars.com/kata/58067088c27998b119000451
 
 // Your challenge is to create a function that takes any number and returns the number that it is a factorial of. So, if your function receives 120, it should return "5!" (as a string).
@@ -17,7 +31,7 @@ function reverseFactorial(num) {
     num /= factorial;
     factorial++;
   }
-  
+
   return factorial === num ? factorial+'!' : 'None';
 }
 
