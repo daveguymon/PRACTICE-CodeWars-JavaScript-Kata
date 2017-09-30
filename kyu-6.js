2b6bbf1225e938494089bda6dbf4bb29703fea9b
Dave Guymon <davidwguymon@gmail.com>
2017-09-30 11:16:48 -0600

    Find the Divisors kyu6

--- kyu-6.js
+++ kyu-6.js
@@ -1,3 +1,26 @@
+// Find the Divisors - https://www.codewars.com/kata/find-the-divisors/javascript
+
+// Create a function named divisors/Divisors that takes an integer and returns an array with all of the integer's divisors(except for 1 and the number itself). If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+//
+// Example:
+//
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+// You can assume that you will only get positive integers as inputs.
+
+function divisors(integer) {
+  let divisors = [];
+
+  for(var i = 2; i < integer; i++) {
+    if(integer % i === 0) {
+      divisors.push(i);
+    }
+    null;
+  }
+  return divisors.length === 0 ? integer+" is prime" : divisors;
+}
+
 //Change It Up - https://www.codewars.com/kata/change-it-up/javascript
 
 // Create a function that takes a string as a parameter and does the following, in this order:
