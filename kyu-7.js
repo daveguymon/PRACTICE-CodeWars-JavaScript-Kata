0373f11fc25653eeed234ed32cfa6490df5aae51
Dave Guymon <davidwguymon@gmail.com>
2017-10-01 22:28:32 -0600

    Simple Interest and Compound Interest kyu7

--- kyu-7.js
+++ kyu-7.js
@@ -1,3 +1,29 @@
+// Simple Interest and Compound Interest - https://www.codewars.com/kata/simple-interest-and-compound-interest/javascript
+
+// Simple interest on a loan is calculated by simply taking the initial amount (the principal, P) and multiplying it by a rate of interest (r) and the number of time periods (n).
+//
+// Compound interest is calculated by adding the interest after each time period to the amount owed, then calculating the next interest payment based on the principal PLUS the interest from all previous periods.
+//
+// Given a principal P, interest rate r, and a number of periods n, return an array [total owed under simple interest, total owed under compound interest].
+//
+// EXAMPLES:
+//
+// interest(100,0.1,1) = [110,110]
+// interest(100,0.1,2) = [120,121]
+// interest(100,0.1,10) = [200,259]
+// Round all answers to the nearest integer. Principal will always be an integer between 0 and 9999; interest rate will be a decimal between 0 and 1; number of time periods will be an integer between 0 and 49.
+
+function interest(P,r,n) {
+  let simple = P + (P * r * n),
+      compound = P;
+
+  for(var i = 0; i < n; i++){
+    compound = compound + compound * r;
+  }
+
+  return [Math.round(simple), Math.round(compound)];
+}
+
 //List Filtering - https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/solutions/javascript
 
 // In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
