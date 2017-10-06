e7ac94f6e5b50ed92987b7d2aa7c8e843e97a900
Dave Guymon <davidwguymon@gmail.com>
2017-10-06 17:06:49 -0600

    Sentence Smash kyu8

--- kyu-8.js
+++ kyu-8.js
@@ -1,4 +1,15 @@
-// No Zeros for Heros - https://www.codewars.com/kata/no-zeros-for-heros/javascript
+// Sentence Smash - https://www.codewars.com/kata/sentence-smash/javascript
+
+// Write a method smash that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+//
+// Example
+//
+// var words = ['hello', 'world', 'this', 'is', 'great'];
+// smash(words); // returns "hello world this is great"
+
+smash = function (words) {
+  return words.join(" ");
+};
 
 // Numbers ending with zeros are boring.
 //
