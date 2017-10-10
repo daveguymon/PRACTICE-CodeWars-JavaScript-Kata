2e2a0ff83a48c244b317013cf35efaf728496507
Dave Guymon <davidwguymon@gmail.com>
2017-10-10 03:07:47 -0600

    Calculate Average kyu8

--- kyu-8.js
+++ kyu-8.js
@@ -1,3 +1,11 @@
+// Calculate Average - https://www.codewars.com/kata/calculate-average/train/javascript
+
+// Write function avg which calculates average of numbers in given list.
+
+function find_average(array) {
+  return array.reduce((a,b) => a + b)/array.length;
+}
+
 // Sentence Smash - https://www.codewars.com/kata/sentence-smash/javascript
 
 // Write a method smash that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
