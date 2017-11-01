4692f6ba29d6f6e30141d627345af6806b794496
Dave Guymon <davidwguymon@gmail.com>
2017-10-31 20:31:52 -0600

    Consecutive Strings

--- kyu-6.js
+++ kyu-6.js
@@ -1,3 +1,27 @@
+// Consecutive Strings - https://www.codewars.com/kata/consecutive-strings/javascript
+
+// You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+//
+// #Example: longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+//
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+
+function longestConsec(strarr, k) {
+  let longestStr = '';
+
+  if(strarr.length === 0 || strarr.length < k || k <= 0){
+    return '';
+  } else {
+    for(var i = 0; i < strarr.length; i++){
+      if(strarr.slice(i, i+k).join('').length > longestStr.length){
+        longestStr = strarr.slice(i, i+k).join('');
+      }
+    }
+
+    return longestStr;
+  }
+}
+
 // Monkey Talk - https://www.codewars.com/kata/monkey-talk/javascript
 
 // Lets talk like a monkey. Find out how! Look at the test cases an engineer code to pass them.
